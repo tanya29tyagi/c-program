5c9f92dd0f99aca33dd8f3fbfa58d7e32b9a77b8
tanya29tyagi <100517596+tanya29tyagi@users.noreply.github.com>
2022-12-11 14:45:57 +0530

    Add files via upload

---
+++ playingwithnumbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define MAX_LEN 100
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char ch;
+    char word[MAX_LEN];
+    char sen [MAX_LEN];
+    scanf("%c",&ch);
+    scanf("%s\n", word);
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n",ch);
+    printf("%s\n",word);
+    printf("%s\n",sen);
+    return 0;
+}
